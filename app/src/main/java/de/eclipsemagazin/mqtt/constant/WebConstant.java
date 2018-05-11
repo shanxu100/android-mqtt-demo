0726df76d81dac0e471a6ea6186d4a908d4458b9
GuanSX <shanxu100@163.com>
2018-05-11 16:49:22 +0800

    windows

---
+++ app/src/main/java/de/eclipsemagazin/mqtt/constant/WebConstant.java
@@ -0,0 +1,10 @@
+package de.eclipsemagazin.mqtt.constant;
+
+/**
+ * @author Guan
+ * @date Created on 2018/5/11
+ */
+public class WebConstant {
+    public static String MQTT_URL = "tcp://139.199.180.158:61613";
+    public static String TOPICS = "testTopic";
+}
